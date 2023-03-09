449fa410e9ce6f9195075479e0c9b9f864bf6cac
muhamadsony <sonyfadhil11@gmail.com>
2023-03-09 19:19:43 +0700

    fix migration

--- ppic/migrations/0011_rename_warehouseproductfix_warehouseproduct.py
+++ ppic/migrations/0011_rename_warehouseproductfix_warehouseproduct.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("ppic", "0010_warehouseproductfix_and_more"),
+        ("ppic", "00101_delete_warehouseproduct"),
     ]
 
     operations = [
